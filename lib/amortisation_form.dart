8c5a7ae2cee5e31c3a8bf14e7af8ec0849ca30af
TikkaToRide <34696597+TikkaToRide@users.noreply.github.com>
2023-02-16 09:51:36 +1030

    Fixed amortisation form positioning

--- lib/amortisation_form.dart
+++ lib/amortisation_form.dart
@@ -11,47 +11,44 @@ class _AmortisationForm extends State<MainForm> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        //unsure if necessary as it contains the column but
-        alignment: Alignment
-            .center, //this does not center the column when in full screen
-        constraints: const BoxConstraints(
-            maxWidth: 500), //however this does cap the width
-        padding: const EdgeInsets.symmetric(
-            vertical: 16), //and this does add padding to the container
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Client Name',
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 500),
+          padding: const EdgeInsets.symmetric(vertical: 32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Client Name',
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Finance Company',
-                  hintText: 'Toyota Finance', //don't know if i should keep
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Finance Company',
+                    hintText: 'Toyota Finance', //don't know if i should keep
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Asset Purchased',
-                  hintText: 'Toyota Camry (S123ABC)',
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Asset Purchased',
+                    hintText: 'Toyota Camry (S123ABC)',
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
